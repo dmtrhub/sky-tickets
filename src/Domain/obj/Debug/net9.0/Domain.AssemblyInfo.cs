db991a5e680730f312ca832717715d3b3b2226ab
Dimitrije Vukovic <dmtr.develop@gmail.com>
2025-03-13 15:27:33 +0100

    Add messaging abstraction and result pattern

--- src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cbbb948fb9a3ee3ce373cd9cf5e39511e39f6d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d3798123bd4555558bf474c1504cbb50a8c5a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ src/Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccd83788119acf5f63649cf9ab602dd9f99eca3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d3798123bd4555558bf474c1504cbb50a8c5a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
